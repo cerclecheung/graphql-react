f8fb9bd4bc2d1fcf1ca7a6f18b91aa4675cbd3b9
Cercle Cheung <43345414+cerclecheung@users.noreply.github.com>
2020-05-21 00:06:48 -0400

    domain seperation in resolvers

--- server/index.js
+++ server/index.js
@@ -1,26 +1,24 @@
 import express from 'express';
-import { ApolloServer, gql } from 'apollo-server-express';
-
-const typeDefs = gql`
-  type Query {
-    me: User
-  }
-  type User {
-    username: String!
-  }
-`;
-
-const resolvers = {
-  Query: {
-    me: () => {
-      return { username: 'Rsssbin Wieruch' };
-    },
-  },
-};
-
-const server = new ApolloServer({ typeDefs, resolvers });
-
+import { ApolloServer } from 'apollo-server-express';
+import schema from './schema';
+import resolvers from './resolvers';
+import models from './models';
+import cors from 'cors';
 const app = express();
+
+app.use(cors());
+
+const typeDefs = schema;
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: {
+    models,
+    me: models.users[1],
+  },
+});
+
 server.applyMiddleware({ app });
 
 app.listen({ port: 4000 }, () =>
