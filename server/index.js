27eb6ff7f2610fc7ce4f6c25d3728a300ae060ce
Cercle Cheung <43345414+cerclecheung@users.noreply.github.com>
2020-05-20 22:18:42 -0400

    set up babel

--- server/index.js
+++ server/index.js
@@ -1,21 +1,21 @@
-const express = require('express');
-const { ApolloServer, gql } = require('apollo-server-express');
+import express from 'express';
+import { ApolloServer, gql } from 'apollo-server-express';
 
 const typeDefs = gql`
-	type Query {
-		me: User
-	}
-	type User {
-		username: String!
-	}
+  type Query {
+    me: User
+  }
+  type User {
+    username: String!
+  }
 `;
 
 const resolvers = {
-	Query: {
-		me: () => {
-			return { username: 'Rsssbin Wieruch' };
-		},
-	},
+  Query: {
+    me: () => {
+      return { username: 'Rsssbin Wieruch' };
+    },
+  },
 };
 
 const server = new ApolloServer({ typeDefs, resolvers });
@@ -23,4 +23,8 @@ const server = new ApolloServer({ typeDefs, resolvers });
 const app = express();
 server.applyMiddleware({ app });
 
-app.listen({ port: 4000 }, () => console.log('Now browse to http://localhost:4000' + server.graphqlPath));
+app.listen({ port: 4000 }, () =>
+  console.log(
+    'Now browse to http://localhost:4000' + server.graphqlPath,
+  ),
+);
